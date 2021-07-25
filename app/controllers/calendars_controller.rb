8434ae51cb06bbde8d9e935f9cdc2c7b2f724e0f
minaema1111 <mina.ema.lol@gmail.com>
2021-07-25 16:16:17 +0900

    Revert "Merge pull request #9 from minaema1111/second_training"
    This reverts commit 66e412de3678a6010eeabaff663346812161a6b1, reversing
    changes made to ea14929a99dc695b0a1d1f219b934fb2abdd087b.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,22 +15,15 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-    # num = Date.today.wday + 1
-    # puts wdays[num]
-
-    # 添字の条件
-    # ①実行した曜日に対応した数値
-    # ②日付が増えるごとに＋１される
-    # ③７を超えた場合はー７される
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)今日が2月1日の場合・・・ Date.today.day => 1日
+    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
@@ -41,14 +34,9 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = (@todays_date + x).wday
-      if wday_num >= 7
-        wday_num = wday_num -7
-      end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
+      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
   end
+end
